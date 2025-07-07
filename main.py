4879c0b40db28132b1ac488576c162e406474663
VroumR <kabal.jewellery@gmail.com>
2025-07-07 20:34:50 +0300

    Update main.py

--- main.py
+++ main.py
@@ -27,4 +27,5 @@ def home():
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
-
+from questions_api import register_question_routes
+register_question_routes(app)
